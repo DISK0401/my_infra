b7360a19f513a193e9eaa2ac15ce099aba61ec48
Daisuke Tomioka <mail@disk0401.net>
2026-05-04 13:58:38 +0900

    import: existing s3 buckets

---
+++ terraform/environments/personal/aws/s3.tf
@@ -0,0 +1,16 @@
+# s3.tf
+resource "aws_s3_bucket" "website" {
+  bucket = "disk0401.net"
+}
+
+resource "aws_s3_bucket" "cloudfront_access_log" {
+  bucket = "cf-accesslog-disk0401.net"
+}
+
+resource "aws_s3_bucket" "cost_usage_report" {
+  bucket = "disk0401-cost-usage-report"
+}
+
+resource "aws_s3_bucket" "cloudtrail" {
+  bucket = "disk0401-ct"
+}
