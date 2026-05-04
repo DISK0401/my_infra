6e1cc9f5bc8ebb7672e9e074e035a08c16546bd2
Daisuke Tomioka <mail@disk0401.net>
2026-05-04 23:54:02 +0900

    delete: importファイル

--- terraform/environments/personal/aws/import.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = aws_cloudtrail.main
-  id = "arn:aws:cloudtrail:ap-northeast-1:${var.aws_account_id}:trail/CloudTrail"
-}
-
-import {
-  to = aws_s3_bucket_policy.cloudtrail
-  id = "disk0401-ct"
-}
