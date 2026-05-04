10f4e4f627a87c77f2f6d73e2b39ba06cc177b09
Daisuke Tomioka <mail@disk0401.net>
2026-05-04 14:08:33 +0900

    fix: add tags to s3 buckets

--- terraform/environments/personal/aws/s3.tf
+++ terraform/environments/personal/aws/s3.tf
@@ -1,10 +1,18 @@
 # s3.tf
 resource "aws_s3_bucket" "website" {
   bucket = "disk0401.net"
+
+  tags = {
+    Name = "MyWebSite"
+  }
 }
 
 resource "aws_s3_bucket" "cloudfront_access_log" {
   bucket = "cf-accesslog-disk0401.net"
+
+  tags = {
+    Name = "MyWebSite"
+  }
 }
 
 resource "aws_s3_bucket" "cost_usage_report" {
@@ -13,4 +21,8 @@ resource "aws_s3_bucket" "cost_usage_report" {
 
 resource "aws_s3_bucket" "cloudtrail" {
   bucket = "disk0401-ct"
+
+  tags = {
+    Name = "CloudTrail"
+  }
 }
