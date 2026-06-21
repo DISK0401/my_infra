9e347fc919f41d81131a394a909138f039c7aff9
Daisuke Tomioka <mail@disk0401.net>
2026-06-21 18:35:34 +0900

    feat: Cloudflareプロバイダの雛形を追加

---
+++ terraform/environments/personal/cloudflare/variables.tf
@@ -0,0 +1,8 @@
+#################################################
+# Cloudflare固有変数
+#################################################
+
+variable "cloudflare_account_id" {
+  description = "CloudflareアカウントID"
+  type        = string
+}
---
+++ terraform/environments/personal/cloudflare/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.8.0"
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5.21"
+    }
+  }
+
+  cloud {
+    organization = "disk0401"
+    workspaces {
+      name = "personal-cloudflare"
+    }
+  }
+}
